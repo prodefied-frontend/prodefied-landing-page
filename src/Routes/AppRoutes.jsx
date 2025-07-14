ac45ee5242e118def25cf533716a3df3eb7c9ec3
Wunderkind23 <johnbrolin30@gmail.com>
2025-07-14 20:54:11 +0100

    saved a file path

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -3,12 +3,14 @@ import SignupPage from "../pages/SignupPage";
 import LoginPage from "../pages/LoginPage";
 import Homepage from "../pages/Homepage";
 import Layout from "../components/Layout";
+import PartnershipPage from "../pages/PartnershipPage";
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route element={<Layout />}>
         <Route path="/" element={<Homepage />} />
+        <Route path="/partnership" element={<PartnershipPage />} />
       </Route>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/sign-up" element={<SignupPage />} />
